f4562cb38a2abd4e30fa1a8d57574929b061b78a
Ricardo Souza <86447805+designer-RAPY@users.noreply.github.com>
2021-10-25 22:33:51 -0300

    update tabelaDeMultiplicacao.js

--- src/tabelaDeMultiplicacao.js
+++ src/tabelaDeMultiplicacao.js
@@ -1,9 +1,21 @@
-function nomeDaFuncao() { 
+function multiplos(numero) { 
     // Substitua "nomeDaSuaFuncao" por um nome mais apropriado
     // TODO: escrever dois loops aninhados para desenhar o array bidimensional e monstar o restante da tabela no console.
-    let x = [];
-    for (let i = 0; i <= 10; i++) {
-        x[i] = [];
+    let output = [];
+    if (numero == undefined){
+        numero = 10;
     }
-    console.table(x)
+
+    for (let linha = 0; linha <= numero; linha++) {
+        output[linha] = [];
+    for (let coluna = 0; coluna <= numero; coluna++){
+        let multiplicacao = linha * coluna
+        output[linha][coluna]= multiplicacao
+
+    }
+    
+ }console.table(output)
+ return output
+
 }
+multiplos(10)
